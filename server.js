c16f5a750df1321252bdba25ae2fc91f3c33694b
Angelina Fabbro <angelina@steamclock.com>
2013-03-21 13:36:35 -0700

    Can connect to postgres, basic routing for express works

--- server.js
+++ server.js
@@ -3,11 +3,38 @@
  */
 
 
-// Dependencies & Initialization
-
+// Express Setup
+console.log("Setting up app...");
 var express = require('express');
-var postgres = require('pg');
 var app = express();
+
+//Postgres Setup
+
+var postgres = require('pg');
+//Just some dumb u/p for now
+var connectionString = "pg://postgres:apple@localhost:5432/emilycarr";
+var client = new postgres.Client(connectionString);
+client.connect(function(err) {
+
+    // Just testing we can connect to the db for now
+    client.query('SELECT NOW() AS "theTime"', function(err, result) {
+      console.log(result.rows[0].theTime);
+    })
+
+    if (err) {
+        console.log(err);
+    }
+});
+
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
+
+
 
 
 
