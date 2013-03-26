836444e6cb98b7db60e6127272031763d34cde81
Angelina Fabbro <angelina@steamclock.com>
2013-03-26 13:26:07 -0700

    initial db migration for emily carr projct

--- migrations/20130326195847-initial-db-design.js
+++ migrations/20130326195847-initial-db-design.js
@@ -2,9 +2,20 @@ var dbm = require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-
+  db.createTable('projects', {
+    id: { type: 'int', primaryKey: true },
+    title: 'string',
+    author: 'string',
+    degreeTrack: 'string',
+    program: 'string',
+    medium: 'string',
+    measurements: 'string',
+    url: 'string',
+    assetUrl: 'string',
+    category: 'string'
+  }, callback);
 };
 
 exports.down = function(db, callback) {
-
+  db.dropTable('projects', callback);
 };
