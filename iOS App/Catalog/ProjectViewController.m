b48bc8d2e5a7ce8fd20b8c122287a2248e44c798
Nigel Brooke <nigel@steamclock.com>
2013-04-10 12:48:18 -0700

    Basic display logic for the pull-at-end to transition between projects in the project view.

--- iOS App/Catalog/ProjectViewController.m
+++ iOS App/Catalog/ProjectViewController.m
@@ -12,6 +12,10 @@
 @property (strong, nonatomic) IBOutlet UIPageControl* pageControl;
 @property (strong, nonatomic) IBOutlet UIView* detailContainer;
 
+@property (strong, nonatomic) IBOutlet UIView* curtain;
+@property BOOL transitionNext;
+@property BOOL doingTransition;
+
 @property (strong, nonatomic) NSDictionary* project;
 
 @property BOOL showingDetails;
@@ -125,11 +129,71 @@
             [self detailButtonPressed:nil];
         }
     }
+    
+    if(!self.doingTransition) {
+        if(!self.curtain) {
+            CGRect curtainFrame = self.scrollView.frame;
+            self.curtain = [[UIView alloc] initWithFrame:curtainFrame];
+            self.curtain.backgroundColor = [UIColor blueColor];
+            [self.view addSubview:self.curtain];
+        }
+        
+        static const int TRANSITION_PULL_START = 150.0f;
+        static const int TRANSITION_PULL_DIST = 150.0f;
+        
+        if((scrollView.contentOffset.x + scrollView.frame.size.width) > scrollView.contentSize.width) {
+            float t = ((scrollView.contentOffset.x + scrollView.frame.size.width) - (scrollView.contentSize.width + TRANSITION_PULL_START)) / (TRANSITION_PULL_DIST);
+            t = MIN(MAX(t, 0.0f), 1.0f);
+            CGRect frame = self.curtain.frame;
+            frame.origin.x = 1024.0f - (t  * 1024.0f);
+            
+            self.curtain.frame = frame;
+            self.transitionNext = YES;
+        }
+        else if(scrollView.contentOffset.x < 0) {
+            float t = ((-scrollView.contentOffset.x - TRANSITION_PULL_START)) / (TRANSITION_PULL_DIST);
+            t = MIN(MAX(t, 0.0f), 1.0f);
+            CGRect frame = self.curtain.frame;
+            frame.origin.x = -1024.0f + (t * 1024.0f);
+            
+            self.curtain.frame = frame;
+            self.transitionNext = NO;
+        }
+    }
+
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    CGRect frame = self.curtain.frame;
+    if(self.transitionNext) {
+        if(frame.origin.x < 512) {
+            frame.origin.x = 0;
+        }
+        else {
+            frame.origin.x = 1024;
+        }
+    }
+    else {
+        if(frame.origin.x > -512) {
+            frame.origin.x = 0;
+        }
+        else {
+            frame.origin.x = -1024;
+        }
+    }
+    
+    self.doingTransition = YES;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        self.curtain.frame = frame;
+    } completion:^(BOOL finished) {
+        self.doingTransition = NO;
+    }];
 }
 
 -(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     if(navigationType == UIWebViewNavigationTypeOther) {
-        // Initial load lett it go
+        // Initial load, let it go
         return YES;
     }
     else
