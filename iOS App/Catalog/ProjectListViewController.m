3ff3ca133b21fb99bbe80cef797c376707d7d860
Nigel Brooke <nigel@steamclock.com>
2013-04-12 14:31:42 -0700

    Better organization of lists. Do degree track though filtering rather than loading new json.

--- iOS App/Catalog/ProjectListViewController.m
+++ iOS App/Catalog/ProjectListViewController.m
@@ -8,6 +8,22 @@
 
 #define REUSE_IDENTIFIER @"ThumbnailCell"
 
+static NSUInteger random_below(NSUInteger n) {
+    NSUInteger m = 1;
+	
+    do {
+        m <<= 1;
+    } while(m < n);
+	
+    NSUInteger ret;
+	
+    do {
+        ret = random() % m;
+    } while(ret >= n);
+	
+    return ret;
+}
+
 @interface ProjectListViewController ()
 @property (strong, nonatomic) IBOutlet UICollectionView* collectionView;
 @property (strong, nonatomic) IBOutlet UIButton* home;
@@ -21,30 +37,28 @@
 @property (strong, nonatomic) UIWebView* aboutWebView;
 
 @property (strong, nonatomic) NSMutableDictionary* cachedImages;
-@property (strong, nonatomic) NSArray* projects;
+@property (strong, nonatomic) NSArray* currentProjects;
 
+@property (strong, nonatomic) NSArray* allProjectsSorted;
+@property (strong, nonatomic) NSArray* allProjectsRandomized;
+
+@property (strong, nonatomic) NSArray* designProjects;
+@property (strong, nonatomic) NSArray* visualArtsProjects;
+@property (strong, nonatomic) NSArray* mediaArtsProjects;
+@property (strong, nonatomic) NSArray* maaProjects;
 
 @end
 
-
 @implementation ProjectListViewController
 
--(NSArray*)sanitizeProjects:(NSArray*)projects {
-    // Strip NSNulls out of the project list
-    NSMutableArray* newArray = [NSMutableArray new];
+#pragma mark Lifecycle
+
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        [self loadProjectLists];
+    }
     
-    [projects enumerateObjectsUsingBlock:^(NSDictionary* obj, NSUInteger idx, BOOL *stop) {
-        NSMutableDictionary* newProject = [NSMutableDictionary new];
-        [obj enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-            if(![obj isKindOfClass:[NSNull class]]) {
-                newProject[key] = obj;
-            }
-        }];
-        
-        [newArray addObject:newProject];
-    }];
-    
-    return newArray;
+    return self;
 }
 
 - (void)viewDidLoad
@@ -70,18 +84,84 @@
     [self.view addSubview:self.aboutWebView];
 }
 
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    [self.cachedImages removeAllObjects];
+}
+
+#pragma mark Loading and Setup
+
+-(NSMutableArray*)sanitizeProjects:(NSArray*)projects {
+    // Strip NSNulls out of the project list
+    NSMutableArray* newArray = [NSMutableArray new];
+    
+    [projects enumerateObjectsUsingBlock:^(NSDictionary* obj, NSUInteger idx, BOOL *stop) {
+        NSMutableDictionary* newProject = [NSMutableDictionary new];
+        [obj enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+            if(![obj isKindOfClass:[NSNull class]]) {
+                newProject[key] = obj;
+            }
+        }];
+        
+        [newArray addObject:newProject];
+    }];
+    
+    return newArray;
+}
+
+
+-(NSArray*)filteredProjectsForDegree:(NSString*)degree {
+    NSMutableArray* filtered = [NSMutableArray new];
+    [self.allProjectsSorted enumerateObjectsUsingBlock:^(NSDictionary* obj, NSUInteger idx, BOOL *stop) {
+        if ([degree isEqualToString:obj[@"degree"]]) {
+            [filtered addObject:obj];
+        }
+    }];
+    return filtered;
+}
+
+-(void)loadProjectLists {
+    NSMutableArray* orig = [self sanitizeProjects:[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"sample" ofType:@"json"]] options:0 error:nil]];
+    
+    srandom((int)[NSDate timeIntervalSinceReferenceDate]);
+    
+    NSMutableArray* randomized = orig;
+    NSMutableArray* sorted = [orig mutableCopy];
+    
+    
+    for(NSUInteger i = [randomized count]; i > 1; i--) {
+        NSUInteger j = random_below(i);
+        [randomized exchangeObjectAtIndex:i-1 withObjectAtIndex:j];
+    }
+    
+    self.allProjectsRandomized = randomized;
+    
+    [sorted sortUsingComparator:^NSComparisonResult(NSDictionary* obj1, NSDictionary* obj2) {
+        return [obj1[@"author"] compare:obj2[@"author"] options:0];
+    }];
+    
+    self.allProjectsSorted = sorted;
+    
+    self.designProjects = [self filteredProjectsForDegree:@"design"];
+    self.visualArtsProjects = [self filteredProjectsForDegree:@"visual-arts"];
+    self.mediaArtsProjects = [self filteredProjectsForDegree:@"media-arts"];
+    self.maaProjects = [self filteredProjectsForDegree:@"masters-of-applied-arts"];
+}
+
+#pragma mark UICollectionViewDelegate
+
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     return 1;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.projects.count;
+    return self.currentProjects.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell* cell = [collectionView dequeueReusableCellWithReuseIdentifier:REUSE_IDENTIFIER forIndexPath:indexPath];
     
-    NSString* imageURL = self.projects[indexPath.row][@"thumbnail"];
+    NSString* imageURL = self.currentProjects[indexPath.row][@"thumbnail"];
     UIImage* cachedimage = self.cachedImages[imageURL];
     
     if(cachedimage) {
@@ -106,25 +186,22 @@
     }
     
     UILabel* title = (UILabel*)[cell viewWithTag:101];
-    title.text = self.projects[indexPath.row][@"title"];
+    title.text = self.currentProjects[indexPath.row][@"title"];
 
     UILabel* author = (UILabel*)[cell viewWithTag:102];
-    author.text = self.projects[indexPath.row][@"author"];
+    author.text = self.currentProjects[indexPath.row][@"author"];
     
     return cell;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    ProjectViewController* project = [[ProjectViewController alloc] initWithProjects:self.projects startIndex:indexPath.row];
+    ProjectViewController* project = [[ProjectViewController alloc] initWithProjects:self.currentProjects startIndex:indexPath.row];
     [self presentViewController:project animated:YES completion:^{
         
     }];
 }
 
--(void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    [self.cachedImages removeAllObjects];
-}
+#pragma mark Navigation bar implimentation
 
 -(void)unselectAll {
     self.home.selected = NO;
@@ -137,86 +214,45 @@
     self.aboutWebView.hidden = YES;
 }
 
--(IBAction)showHome:(id)sender {
-    if(self.home.selected) {
+-(void)showProjectList:(NSArray*)projects forButton:(UIButton*)button {
+    if(button.selected) {
         return;
     }
     
     [self unselectAll];
-    self.home.selected = YES;
     
-    self.projects = [self sanitizeProjects:[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"sample" ofType:@"json"]] options:0 error:nil]];
+    button.selected = YES;
+    
+    self.currentProjects = projects;;
     [self.collectionView reloadData];
+}
+
+-(IBAction)showHome:(id)sender {
+    [self showProjectList:self.allProjectsRandomized forButton:self.home];
 }
 
 -(IBAction)showDesign:(id)sender {
-    if(self.design.selected) {
-        return;
-    }
-    
-    [self unselectAll];
-    self.design.selected = YES;
-    
-    self.projects = [self sanitizeProjects:[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"sample2" ofType:@"json"]] options:0 error:nil]];
-    [self.collectionView reloadData];
+    [self showProjectList:self.designProjects forButton:self.design];
 }
 
 -(IBAction)showVisualArts:(id)sender {
-    if(self.visualArts.selected) {
-        return;
-    }
-
-    [self unselectAll];
-    self.visualArts.selected = YES;
-    
-    self.projects = [self sanitizeProjects:[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"sample3" ofType:@"json"]] options:0 error:nil]];
-    [self.collectionView reloadData];
+    [self showProjectList:self.visualArtsProjects forButton:self.visualArts];
 }
 
 -(IBAction)showMediaArts:(id)sender {
-    if(self.mediaArts.selected) {
-        return;
-    }
-    
-    [self unselectAll];
-    self.mediaArts.selected = YES;
-    
-    self.projects = [self sanitizeProjects:[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"sample4" ofType:@"json"]] options:0 error:nil]];
-    [self.collectionView reloadData];
+    [self showProjectList:self.mediaArtsProjects forButton:self.mediaArts];
 }
 
 -(IBAction)showMAA:(id)sender {
-    if(self.MAA.selected) {
-        return;
-    }
-
-    [self unselectAll];
-    self.MAA.selected = YES;
-    
-    self.projects = [self sanitizeProjects:[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"sample5" ofType:@"json"]] options:0 error:nil]];
-    [self.collectionView reloadData];
+    [self showProjectList:self.maaProjects forButton:self.MAA];
 }
 
 -(IBAction)showSearch:(id)sender {
-    if(self.search.selected) {
-        return;
-    }
-    
-    [self unselectAll];
-    self.search.selected = YES;
-    
-    self.projects = [self sanitizeProjects:[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"sample" ofType:@"json"]] options:0 error:nil]];
-    [self.collectionView reloadData];
+    [self showProjectList:self.allProjectsSorted forButton:self.search];
 }
 
 -(IBAction)showAbout:(id)sender {
-    if(self.about.selected) {
-        return;
-    }
-    
-    [self unselectAll];
-    
-    self.about.selected = YES;
+    [self showProjectList:nil forButton:self.about];
     self.aboutWebView.hidden = NO;
 }
 
