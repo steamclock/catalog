5dd52b36648e69086230a8d945cd755adb763259
Angelina Fabbro <angelina@steamclock.com>
2013-04-12 14:33:36 -0700

    Fix problem when new-named assets are not in an array from edit form

--- Web App/routes/edit.js
+++ Web App/routes/edit.js
@@ -68,7 +68,7 @@ exports.get = function(req, res){
 
 
 exports.update = function(req, res){
-    console.log(req.body);
+    console.log(req.files);
     console.log("-------------------");
     async.waterfall([
         function(callback){
@@ -92,7 +92,25 @@ exports.update = function(req, res){
            for (var key in req.files) {
                 if (req.files.hasOwnProperty(key)) {
                     if (key === "new") {
-                        req.files.new.forEach(function(file){
+                        if (req.files.new instanceof Array) {
+                            req.files.new.forEach(function(file){
+                                if (file.type === "image/jpeg") {
+                                    imagemagick.identify(file.path, function(err, features){
+                                        if(err){console.log(err)};
+                                        var accept = ((features.width >= 1500) || (features.height >=1500));
+                                        if (!accept){
+                                            var formValues = JSON.stringify(req.body);
+                                            res.flash('message','One of your images did not meet the minimum dimensions. Please verify the dimensions of all of your assets.');
+                                            res.render('edit/edit', { title : "Error in submission", formData : formValues });
+                                            callback(true); //Exits waterfall
+                                        } else {
+                                            callback(null);
+                                        }
+                                    });  
+                                }
+                            });
+                        } else {
+                            var file = req.files.new;
                             if (file.type === "image/jpeg") {
                                 imagemagick.identify(file.path, function(err, features){
                                     if(err){console.log(err)};
@@ -107,7 +125,7 @@ exports.update = function(req, res){
                                     }
                                 });  
                             }
-                        });
+                        }
                     } else {
                         var file = req.files[key];
                         if (file.type === "image/jpeg") {
@@ -133,43 +151,81 @@ exports.update = function(req, res){
             for (var key in req.files) {
                 if (req.files.hasOwnProperty(key)) {
                     if (key === "new") {
-                        req.files.new.forEach(function(file) {
-                            if (file.name) { 
-                                // get the temporary location of the file
-                                var tmp_path = file.path;
-                                // set where the file should actually exists - in this case it is in the "images" directory
-                                var target_path = './public/images/projects/' + file.name;
-                                // move the file from the temporary location to the intended location
-                                fs.readFile(file.path, function (err, data) {
-                                      fs.writeFile(target_path, data, function (err) {
-                                        if (err) {
-                                            console.log("Error:" + err)
-                                        } else {
-                                            console.log("File copied");
-                                            fs.unlink(tmp_path, function(err){
+                            if (req.files.new instanceof Array) {
+                                req.files.new.forEach(function(file) {
+                                    if (file.name) { 
+                                        // get the temporary location of the file
+                                        var tmp_path = file.path;
+                                        // set where the file should actually exists - in this case it is in the "images" directory
+                                        var target_path = './public/images/projects/' + file.name;
+                                        // move the file from the temporary location to the intended location
+                                        fs.readFile(file.path, function (err, data) {
+                                              fs.writeFile(target_path, data, function (err) {
                                                 if (err) {
-                                                    console.log("Error:" + err);
+                                                    console.log("Error:" + err)
+                                                } else {
+                                                    console.log("File copied");
+                                                    fs.unlink(tmp_path, function(err){
+                                                        if (err) {
+                                                            console.log("Error:" + err);
+                                                        }
+                                                    });
                                                 }
-                                            });
-                                        }
-                                      });
-                                }); // End fs read/write 
-                                var localFileURL = "/public/images/projects/" + file.name;
+                                              });
+                                        }); // End fs read/write 
+                                        var localFileURL = "/public/images/projects/" + file.name;
 
-                                var assetInsertion = client.query(
-                                    "INSERT into assets(projectid, type, url) values($1, $2, $3)",
-                                    [req.body.project, "image", localFileURL]
-                                );
+                                        var assetInsertion = client.query(
+                                            "INSERT into assets(projectid, type, url) values($1, $2, $3)",
+                                            [req.body.project, "image", localFileURL]
+                                        );
 
-                                assetInsertion.on('error', function(error) {
-                                    console.log("Error: " + error)
-                                });        
+                                        assetInsertion.on('error', function(error) {
+                                            console.log("Error: " + error)
+                                        });        
 
-                                assetInsertion.on('end', function(result){
-                                    console.log("Inserted image into assets table");
-                                });
+                                        assetInsertion.on('end', function(result){
+                                            console.log("Inserted image into assets table");
+                                        });
+                                    }
+                                }); //End forEach  
+                            } else {
+                                if (file.name) { 
+                                    // get the temporary location of the file
+                                    var tmp_path = file.path;
+                                    // set where the file should actually exists - in this case it is in the "images" directory
+                                    var target_path = './public/images/projects/' + file.name;
+                                    // move the file from the temporary location to the intended location
+                                    fs.readFile(file.path, function (err, data) {
+                                          fs.writeFile(target_path, data, function (err) {
+                                            if (err) {
+                                                console.log("Error:" + err)
+                                            } else {
+                                                console.log("File copied");
+                                                fs.unlink(tmp_path, function(err){
+                                                    if (err) {
+                                                        console.log("Error:" + err);
+                                                    }
+                                                });
+                                            }
+                                          });
+                                    }); // End fs read/write 
+                                    var localFileURL = "/public/images/projects/" + file.name;
+
+                                    var assetInsertion = client.query(
+                                        "INSERT into assets(projectid, type, url) values($1, $2, $3)",
+                                        [req.body.project, "image", localFileURL]
+                                    );
+
+                                    assetInsertion.on('error', function(error) {
+                                        console.log("Error: " + error)
+                                    });        
+
+                                    assetInsertion.on('end', function(result){
+                                        console.log("Inserted image into assets table");
+                                    });
+                                } 
                             }
-                        }); //End forEach
                     } else {
                         var file = req.files[key];
                         if (file.name) { 
