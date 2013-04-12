e97e35d46a0fe480f912c961164b8942683c4382
Angelina Fabbro <angelina@steamclock.com>
2013-04-11 19:54:55 -0700

    Project details updated, now to update assets

--- Web App/routes/edit.js
+++ Web App/routes/edit.js
@@ -74,7 +74,7 @@ exports.update = function(req, res){
         function(callback){
             var token = req.headers.referer.substring(req.headers.referer.lastIndexOf('/') + 1);
             var query = client.query(
-                "UPDATE projects SET title = $2, author = $3, website = $4, degree = $5, medium = $6, measurements = $7 WHERE token = $1", 
+                "UPDATE projects SET title = $2, author = $3, website = $4, degree = $5, medium = $6, measurements = $7, published = false WHERE token = $1", 
                 [token, req.body.title, req.body.author, req.body.website, req.body.degree, req.body.medium, req.body.measurements]);
 
             query.on('error', function(error){
