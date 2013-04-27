17d2396b461e4eb6cffeaca484854d1f318c3393
Angelina Fabbro <angelina@steamclock.com>
2013-04-26 18:11:23 -0700

    Fix routes to avoid http auth for admin approve/reject routes

--- Web App/app.js
+++ Web App/app.js
@@ -77,8 +77,8 @@ app.get('/admin', function(req, res){
         admin.get(req, res);
     });
 });
-app.post('/admin/approve/:id', admin.approve);
-app.post('/admin/reject/:id', admin.reject);
+app.post('/approve/project/:id', admin.approve);
+app.post('/approve/project/:id', admin.reject);
 
 
 // Degree page that lists all projects for a given degree
